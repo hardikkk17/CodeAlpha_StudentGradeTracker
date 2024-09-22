1748689a43abfcfd7ba8fc7d98f483f3ec285440
hardikkk17 <156995001+hardikkk17@users.noreply.github.com>
2024-09-22 12:12:22 +0530

    Update Studentgradetracker.java

--- Studentgradetracker.java
+++ Studentgradetracker.java
@@ -1,1 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> grades = new ArrayList<>();
+
+        System.out.println("Enter the number of students:");
+        int numStudents = scanner.nextInt();
+
+        for (int i = 0; i < numStudents; i++) {
+            System.out.println("Enter the grade of student " + (i + 1) + ":");
+            grades.add(scanner.nextInt());
+        }
+
+        int sum = 0;
+        int highest = grades.get(0);
+        int lowest = grades.get(0);
+
+        for (int grade : grades) {
+            sum += grade;
+            if (grade > highest) {
+                highest = grade;
+            }
+            if (grade < lowest) {
+                lowest = grade;
+            }
+        }
+
+        double average = (double) sum / grades.size();
+
+        System.out.println("Highest grade: " + highest);
+        System.out.println("Lowest grade: " + lowest);
+        System.out.printf("Average grade: %.2f", average);
+    }
+}
